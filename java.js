31737acd005d1cde9d55db82f0ddad96a0f00030
moizhere1924-debug <moizhere1924@gmail.com>
2025-11-16 14:25:48 +0500

    java

---
+++ java.js
@@ -0,0 +1,44 @@
+let firstname = prompt("Enter Your First Name");
+console.log(`First Name is: ${firstname}`);
+
+
+let lastname = prompt("Enter Your Last Name");
+console.log(`Last Name is: ${lastname}`);
+
+
+
+let fullname = prompt("Enter Your Full Name");
+console.log(`Full Name is: ${fullname}`);
+
+
+let age = prompt("Enter Your Age");
+console.log(`Age is: ${age}`);
+
+
+let city = prompt("Enter Your City Name");
+console.log(`City Name is: ${city}`);
+
+
+let country = prompt("Enter Your Country Name");
+console.log(`Country Name is: ${country}`);
+
+
+let college = prompt("Enter Your College/School Name");
+console.log(`College/School Name is: ${college}`);
+
+
+let favoriteSubject = prompt("Enter Your Favorite Subject");
+console.log(`Favorite Subject is: ${favoriteSubject}`);
+
+
+let dateofbirth = prompt("Enter Your Date of Birth");
+console.log(`Date of Birth is: ${dateofbirth}`);
+
+
+let currentyear = prompt("Enter Your Current Year");
+console.log(`Current Year is: ${currentyear}`);
+
+
+
+
+
